62af259ece9dd8f3e75bf8e1f1ea065cff2f4f6b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:19 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.sentinel_data_connector_threat_intelligence_taxiis : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
+data "azurerm_key_vault_secret" "user_name" {
+  for_each     = { for k, v in var.sentinel_data_connector_threat_intelligence_taxiis : k => v if v.user_name_key_vault_id != null && v.user_name_key_vault_secret_name != null }
+  name         = each.value.user_name_key_vault_secret_name
+  key_vault_id = each.value.user_name_key_vault_id
+}
 resource "azurerm_sentinel_data_connector_threat_intelligence_taxii" "sentinel_data_connector_threat_intelligence_taxiis" {
   for_each = var.sentinel_data_connector_threat_intelligence_taxiis
 
@@ -7,9 +17,9 @@ resource "azurerm_sentinel_data_connector_threat_intelligence_taxii" "sentinel_d
   log_analytics_workspace_id = each.value.log_analytics_workspace_id
   name                       = each.value.name
   lookback_date              = each.value.lookback_date
-  password                   = each.value.password
+  password                   = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
   polling_frequency          = each.value.polling_frequency
   tenant_id                  = each.value.tenant_id
-  user_name                  = each.value.user_name
+  user_name                  = each.value.user_name != null ? each.value.user_name : try(data.azurerm_key_vault_secret.user_name[each.key].value, null)
 }
 
--- variables.tf
+++ variables.tf
@@ -10,22 +10,30 @@ Required:
 Optional:
     - lookback_date
     - password
+    - password_key_vault_id (alternative to password - read from Key Vault instead)
+    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
     - polling_frequency
     - tenant_id
     - user_name
+    - user_name_key_vault_id (alternative to user_name - read from Key Vault instead)
+    - user_name_key_vault_secret_name (alternative to user_name - read from Key Vault instead)
 EOT
 
   type = map(object({
-    api_root_url               = string
-    collection_id              = string
-    display_name               = string
-    log_analytics_workspace_id = string
-    name                       = string
-    lookback_date              = optional(string) # Default: "01/01/1970 00:00:00"
-    password                   = optional(string)
-    polling_frequency          = optional(string) # Default: "OnceAnHour"
-    tenant_id                  = optional(string)
-    user_name                  = optional(string)
+    api_root_url                    = string
+    collection_id                   = string
+    display_name                    = string
+    log_analytics_workspace_id      = string
+    name                            = string
+    lookback_date                   = optional(string) # Default: "01/01/1970 00:00:00"
+    password                        = optional(string)
+    password_key_vault_id           = optional(string)
+    password_key_vault_secret_name  = optional(string)
+    polling_frequency               = optional(string) # Default: "OnceAnHour"
+    tenant_id                       = optional(string)
+    user_name                       = optional(string)
+    user_name_key_vault_id          = optional(string)
+    user_name_key_vault_secret_name = optional(string)
   }))
 }
 
