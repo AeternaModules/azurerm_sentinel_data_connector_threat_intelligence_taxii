414ba3ba231ade7e98408aa2f6c5a78e5333053d
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:37:47 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -25,11 +25,11 @@ EOT
     display_name                    = string
     log_analytics_workspace_id      = string
     name                            = string
-    lookback_date                   = optional(string) # Default: "01/01/1970 00:00:00"
+    lookback_date                   = optional(string)
     password                        = optional(string)
     password_key_vault_id           = optional(string)
     password_key_vault_secret_name  = optional(string)
-    polling_frequency               = optional(string) # Default: "OnceAnHour"
+    polling_frequency               = optional(string)
     tenant_id                       = optional(string)
     user_name                       = optional(string)
     user_name_key_vault_id          = optional(string)
